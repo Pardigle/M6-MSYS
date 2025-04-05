44f1cf586f30c269f9dacbe902b816f17a8be252
pardige <140949466+Pardigle@users.noreply.github.com>
2025-04-05 21:24:28 +0800

    Changed urls

--- M6E-DjangoForms-AlzateLacsonPitogo/tapas/tapasapp/models.py
+++ M6E-DjangoForms-AlzateLacsonPitogo/tapas/tapasapp/models.py
@@ -11,3 +11,15 @@ class Dish(models.Model):
     def __str__(self):
         return str(self.pk) + ": " + self.name
     
+class Account(models.Model):
+    username = models.CharField(max_length=300)
+    password = models.CharField(max_length=300)
+
+    def getUsername(self):
+        return self.username
+    
+    def getPassword(self):
+        return self.password
+    
+    def __str__(self):
+        return str(self.pk) + ": " + self.username
